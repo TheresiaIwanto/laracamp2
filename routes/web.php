663d9a3b7024ad8be60e583db29283bef3d0fd14
Theresia Iwanto <theresia.422021019@civitas.ukrida.ac.id>
2023-01-10 16:54:57 +0700

    integration landing page, checkout, and my dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+// use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,21 +25,31 @@ Route::get('/', function () {
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('checkout', function () {
-    return view('checkout');
-})->name('checkout');
+// Route::get('checkout/{camp:slug}', function () {
+//     return view('checkout');
+// })->name('checkout');
 
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+Route::middleware(['auth'])->group(function(){
+
+    // checkout routes
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+    // user dasdhboard
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+});
 
 
+// Route::get('success-checkout', function () {
+//     return view('success_checkout');
+// })->name('success-checkout');
 
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
